d79e046f749f630dbd66e3d001c02a306c4ee461
Ryan Achmad <rianacmad@gmail.com>
2025-09-05 23:24:43 +0700

    Language change

--- cleanup.php
+++ cleanup.php
@@ -1,13 +1,13 @@
 <?php
-// Skrip ini untuk dijalankan oleh Cron Job
+// This script is to be run by Cron Job.
 
 $base_dir = __DIR__ . '/downloader/';
-$max_age_seconds = 3600; // 1 jam (60 detik * 60 menit)
+$max_age_seconds = 3600; // 1 hour (60 seconds * 60 minutes)
 
-// Direktori yang akan dibersihkan
+// Directories to be cleaned
 $dirs_to_clean = [$base_dir . 'mp3', $base_dir . 'mp4'];
 
-echo "Memulai proses pembersihan...\n";
+echo "[*] Starting the cleaning process...\n";
 
 foreach ($dirs_to_clean as $dir) {
     if (!is_dir($dir)) continue;
@@ -20,13 +20,13 @@ foreach ($dirs_to_clean as $dir) {
         // Hapus file jika lebih tua dari $max_age_seconds
         if (time() - filemtime($file_path) > $max_age_seconds) {
             if (unlink($file_path)) {
-                echo "Menghapus: $file_path\n";
+                echo "Deleting: $file_path\n";
             } else {
-                echo "Gagal menghapus: $file_path\n";
+                echo "Failed to delete: $file_path\n";
             }
         }
     }
 }
 
-echo "Pembersihan selesai.\n";
+echo "Cleaning Completed.\n";
 ?>
